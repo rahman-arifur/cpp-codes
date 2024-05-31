435daa3c7e8e5b8302d229fd78295a20c522f360
rahman-arifur <113102124+rahman-arifur@users.noreply.github.com>
2024-06-01 05:44:36 +0600

    dijkstra.cpp

---
+++ dijkstra.cpp
@@ -0,0 +1,19 @@
+    vector<int> graph[MAXN];
+    vector<int> dis(MAXN, 1e9 + 7);
+    using pii = pair<int, int>;
+    void dijkstra(int src){
+        priority_queue<pii, vector<pii>, greater<pii> > pq;
+        pq.push({0, src});
+        // pair = weght, destination
+        dis[src] = 0;
+        while(!pq.empty()){
+            from = pq.top().second, d = pq.top().first;
+            pq.pop();
+            if(dis[from] < d) continue;
+            for(auto& [wt, to] : graph[from]){
+                if(wt + dis[from] < dis[to])
+                        dis[to] = wt + dis[from],
+                        pq.push({dis[to], to});
+            }
+        }
+    }
