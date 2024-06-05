b166c09c18cc29020c4c6d6eb0a47f48b4cb07e8
rahman-arifur <113102124+rahman-arifur@users.noreply.github.com>
2024-06-05 09:44:09 +0600

    articulation_bridges.cpp

--- articulation_bridges.cpp
+++ articulation_bridges.cpp
@@ -1,17 +1,17 @@
 vector<vector<int>> adj; // adjacency list of graph
 
-vector<bool> visited;
+vector<bool> vis;
 vector<int> tin, low;
 int timer;
 
 void dfs(int v, int p = -1) {
-    visited[v] = true;
+    vis[v] = true;
     tin[v] = low[v] = timer++;
     for (int to : adj[v]) {
         if (to == p) continue;
-        if (visited[to]) {
+        if (vis[to]) 
             low[v] = min(low[v], tin[to]);
-        } else {
+        else {
             dfs(to, v);
             low[v] = min(low[v], low[to]);
             if (low[to] > tin[v])
