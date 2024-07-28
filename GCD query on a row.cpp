b61777c09ccedd6e860dfb293433203b92fcd157
rahman-arifur <113102124+rahman-arifur@users.noreply.github.com>
2024-07-28 16:49:51 +0600

    Create GCD query on a row.cpp

---
+++ GCD query on a row.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll=long long; 
+// https://codeforces.com/contest/1458/problem/A
+// Row GCD
+// given X, print gcd of (X + a1, X + a2, ... , X + an) for Q queries N < 100000
+// gcd(x, y) = gcd(x - y, y) 
+ll GCD(ll a, ll b) {
+	for(;b;a%=b,swap(a,b));
+	return a;
+}
+signed main() 
+{
+    cin.tie(nullptr) -> sync_with_stdio(false);
+    /*   tophhh   */
+    int n, Q;
+    ll G = 0, x ,i;
+    cin >> n >> Q;
+    for(ll i = 0, k; i < n; i++) {
+    	cin >> k;
+    	if(i) G = GCD(G, abs(k - x));
+    	else x = k;
+    }
+    while(Q--) {
+    	cin >> i;
+    	cout << GCD(G, i + x) << ' ';
+    }
+return 0;
+}
