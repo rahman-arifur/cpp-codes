27d22971df63758a64434b69c6c4b2f5016f145d
rahman-arifur <113102124+rahman-arifur@users.noreply.github.com>
2024-07-29 18:19:10 +0600

    Create Catalan Numbers(n*n).cpp

---
+++ Catalan Numbers(n*n).cpp
@@ -0,0 +1,8 @@
+ll cat[1001];
+    cat[0] = cat[1] = 1;
+    for(int i = 2; i <= 1000; i++) {
+        auto &res = cat[i];
+        for(int j = 0; j < i; j++)
+            res += cat[j] * cat[i - j - 1],
+            res %= Mod;
+    }
