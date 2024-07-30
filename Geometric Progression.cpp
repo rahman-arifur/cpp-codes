d07cc2be6a837e9ce59e26b9e12bf0e39b50760f
rahman-arifur <113102124+rahman-arifur@users.noreply.github.com>
2024-07-30 16:36:07 +0600

    Update Geometric Progression.cpp

--- Geometric Progression.cpp
+++ Geometric Progression.cpp
@@ -1,33 +1,42 @@
-#include "bits/stdc++.h"
+#include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
 // https://atcoder.jp/contests/abc293/tasks/abc293_e
 // find sum of pow(A, i) for all 0 <= i < X modulo M
+// a^0 + a^1 + ... + a^x (under Mod m)
 // X = 1e12, A, M = 1e9
 int Mod;
-// x >> 1 means x / 2
 ll powMod(ll a, ll b) {
     ll ans = 1 % Mod;
     for (a %= Mod; b; a = a * a % Mod, b >>= 1)
         if (b & 1) ans=(ans*a)%Mod;
     return ans;
 }
-ll geoPro(ll a, ll b) {
-    if(b == 1) return 1 % Mod;
-    ll tem = geoPro(a, b >> 1) * (1 + powMod(a, b >> 1)) % Mod;
-    if(b & 1) return (tem + powMod(a, b - 1)) % Mod;
-    return tem;
+/*
+proof : 
+		if(x is odd like 5)
+			a^0 + a^1 + a^2 + a^3 + a^4 + a^5
+		=   (a^0 + a^1 + a^2) +  a^3 * (a^0 + a^1 + a^2)
+		= 	cur + a^(5 / 2 + 1) * cur
+
+		else (x is even like 6)
+			a^0 + a^1 + a^2 + a^3 + a^4 + a^5 + a^6
+		=	sum(0 .. 5) + a^6
+*/
+int sum(ll x, int a) {
+  if(x == 0) return 1 % Mod;
+  if (x & 1) {
+    ll p = x / 2;
+    int cur = sum(p, a);
+    return (cur + 1LL * powMod(a, p + 1) * cur % Mod) % Mod;
+  }
+  return (sum(x - 1, a) + powMod(a, x)) % Mod;
 }
-signed main()
-{
-    cin.tie(nullptr)->sync_with_stdio(false);
-    /*   tophhh   */
-    ll A, X;
-    cin >> A >> X >> Mod;
-    if (A == 1)
-        return cout << X % Mod, 0;
-
-    cout << geoPro(A, X);
-
-    return 0;
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int a; ll x; 
+  cin >> a >> x >> Mod;
+  cout << sum(x - 1, a) << '\n';
+  return 0;
 }
