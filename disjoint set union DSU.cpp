7993428d7436a4d424311dfbde96cbb1c831b66c
rahman-arifur <113102124+rahman-arifur@users.noreply.github.com>
2024-08-31 20:01:37 +0600

    Create disjoint set union DSU.cpp

---
+++ disjoint set union DSU.cpp
@@ -0,0 +1,24 @@
+struct DSU{
+    vector<int> parent, sizes;
+    int n;
+    DSU(int c) : n(c) {
+        parent.resize(n + 1);
+        iota(begin(parent), end(parent), 0);
+        sizes = vector<int> (n + 1, 0);
+    }
+    int find(int v) {
+        while(v != parent[v])
+            v = parent[v] = parent[parent[v]];
+        return v;
+    }
+    bool same(int u, int v) {
+        return find(u) == find(v);
+    }
+    void unite(int u, int v) {
+        u = find(u), v = find(v);
+        if(u == v) return;
+        if(sizes[u] > sizes[v]) swap(u, v);
+        parent[u] = v;
+        sizes[v] += sizes[u];
+    }
+};
