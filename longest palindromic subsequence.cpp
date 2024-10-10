33524937acf82f1a4c0b3fcec4c06da362218b95
rahman-arifur <113102124+rahman-arifur@users.noreply.github.com>
2024-10-10 19:01:51 +0600

    Update and rename D_Generating_Palindromes.cpp to longest palindromic subsequence.cpp

--- longest palindromic subsequence.cpp
+++ longest palindromic subsequence.cpp
@@ -10,12 +10,14 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 
 const int Maxn = 2e5 + 10, Mod = 1e9 + 7;
 const ll Inf = 1e18;
-
+// this question answers how many characters u need to insert to make string S a palindrome
+// so we do suze(S) - (longest palindromic subsequence) as the answer
 int solve() {
     string s; 
     cin >> s;
     int n = size(s);
     vector dp(n + 1, vector<int> (n + 1, -1));
+    // this function calculates the longest palindromic subsequence in a string in range i ... j
     function<int(int, int)> mem = [&](int i, int j) -> int {
         if (i == j) return dp[i][j] = 1;
         if (-1 != dp[i][j]) return dp[i][j];
