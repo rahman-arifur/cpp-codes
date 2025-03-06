2e10b48790684c7d12211c082f5624b2589892bb
Md. Arifur Rahman <113102124+rahman-arifur@users.noreply.github.com>
2025-03-07 05:00:53 +0600

    Update Manachers.cpp

--- Manachers.cpp
+++ Manachers.cpp
@@ -1,6 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> manacher(string& s) {
+    string t = "#";
+    for (auto c : s) {
+        t += c;
+        t += '#';
+    }
+    int n = t.size();
+    vector<int> r(n);
+    for (int i = 0, j = 0; i < n; i++) {
+        if (2 * j - i >= 0 && j + r[j] > i) {
+            r[i] = min(r[2 * j - i], j + r[j] - i);
+        }
+        while (i - r[i] >= 0 && i + r[i] < n && t[i - r[i]] == t[i + r[i]]) {
+            r[i] += 1;
+        }
+        if (i + r[i] > j + r[j]) {
+            j = i;
+        }
+    }
+    return r;
+}
+
 struct Manacher {
   vector<int> p[2];
   // p[1][i] = (max odd length palindrome centered at i) / 2 [floor division]
