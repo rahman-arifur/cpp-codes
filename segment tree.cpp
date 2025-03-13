1de9b67bf4e538a435eb20c4414e2574c20b50be
Md. Arifur Rahman <113102124+rahman-arifur@users.noreply.github.com>
2025-03-14 05:39:27 +0600

    Update segment tree.cpp

--- segment tree.cpp
+++ segment tree.cpp
@@ -1,59 +1,95 @@
-
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
 template <typename T>
 class SegmentTree {
 private:
-	vector<T> tree, arr;
-	int n;
+    // everything is 0 index
+    vector<T> tree, arr;
+    int n;
+    function<T(T&,T&)> func; // merge function 
+    // for sum, xor, min max etc
+
     T E; // when out of range what value to return
     // ex: if out of range for range minimum E=+inf
     // if xor E=0;
-    #define left(x) ((x << 1) | 1)
-    #define right(x) ((x << 1) + 2)
-	int mid(int l, int r) { return r + l >> 1; }
-	T func(T a, T b) { 
-		return a & b; // make it add, min, max, xor
-	}
-	void build(int nd, int st, int ed) {
-		if (st == ed) return void(tree[nd] = arr[st]);
-		int m = mid(st, ed);
-		build(left(nd), st, m);
-		build(right(nd), m + 1, ed);
-		tree[nd] = func(tree[left(nd)], tree[right(nd)]);
-	}
+    void build(int nd, int st, int ed) {
+        if (st == ed) return void(tree[nd] = arr[st]);
+        int m = st+ed>>1, l=(nd<<1)|1, r=l+1;
+        build(l, st, m);
+        build(r, m + 1, ed);
+        tree[nd] = func(tree[l], tree[r]);
+    }
 
-	void update(int nd, int st, int ed, int idx, T val) {
-		if (st == ed) {
-			arr[idx] = val;
-			tree[nd] = val;
-			return;
-		}
-		int m = mid(st, ed);
-		if (idx <= m)
-			update(left(nd), st, m, idx, val);
-		else
-			update(right(nd), m + 1, ed, idx, val);
-		tree[nd] = func(tree[left(nd)], tree[right(nd)]);
-	}
+    void update(int nd, int st, int ed, int idx, T val) {
+        if (st == ed) {
+            arr[idx] = val;
+            tree[nd] = val;
+            return;
+        }
+        int m = st+ed>>1, l=(nd<<1)|1, r=l+1;
+        if (idx <= m)
+            update(l, st, m, idx, val);
+        else
+            update(r, m + 1, ed, idx, val);
+        tree[nd] = func(tree[l], tree[r]);
+    }
 
-	T query(int nd, int st, int ed, int l, int r) {
-		if (r < st || ed < l) return E;
-		if (l <= st && ed <= r) return tree[nd];
-		int m = mid(st, ed);
-		T L = query(left(nd), st, m, l, r);
-		T R = query(right(nd), m + 1, ed, l, r);
-		return func(L, R);
-	}
+    T query(int nd, int st, int ed, int L, int R) {
+        if (R < st || ed < L) return E;
+        if (L <= st && ed <= R) return tree[nd];
+        int m = st+ed>>1, l=(nd<<1)|1, r=l+1;
+        T Lq = query(l, st, m, L, R);
+        T Rq = query(r, m + 1, ed, L, R);
+        return func(Lq, Rq);
+    }
 
 public:
-	SegmentTree(const vector<T>& a, T E)
-		: arr(a), E(E), n(a.size()) {
-		tree.resize(4 * n, E);
-		build(0, 0, n - 1);
-	}
-	void update(int idx, T val) {
-		update(0, 0, n - 1, idx, val);
-	}
+    // array, null value, merge function
+    SegmentTree(vector<T>& a, T nll, function<T(T&,T&)> ff) {
+        func=ff; E=nll;
+        swap(a,arr);
+        n=arr.size();
+        tree.resize(4 * n, E);
+        build(0, 0, n - 1);
+    }
+    void update(int idx, T val) {
+        update(0, 0, n - 1, idx, val);
+    }
     T query(int l, int r) {
-		return query(0, 0, n - 1, l, r);
-	}
+        return query(0, 0, n - 1, l, r);
+    }
 };
+// min->nll=inf, mx->nll= -inf
+// xor->nll=0, and->nll=LLONG_MAX(all bits set)
+// take input as arr=vector<T>
+
+// function<ll(ll&, ll&)> ff = [](ll& a, ll& b) {
+//         return a+b;
+// };
+// SegmentTree<ll> sg(a, 0, ff);
+
+
+// https://cses.fi/problemset/task/1648/
+
+int main() {
+    int n,q;cin>>n>>q;
+    vector<ll> a(n);
+    for(ll& i:a)cin>>i; 
+    function<ll(ll&, ll&)> ff = [](ll& a, ll& b) {
+        return a+b;
+    };
+    SegmentTree<ll> sg(a, 0, ff);
+    for(int l,r,t,v;q--;) {
+        cin>>t;
+        if(t==1) {
+            cin>>l>>v; l--;
+            sg.update(l, v);
+        }
+        else {
+            cin>>l>>r; l--, r--;
+            cout<<sg.query(l,r)<<"\n";
+        }
+    }
+    return 0;
+}
