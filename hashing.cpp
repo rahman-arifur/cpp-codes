0e21c383da77a6c30a5a67b42d1726075af220b6
Md. Arifur Rahman <113102124+rahman-arifur@users.noreply.github.com>
2025-03-16 21:21:58 +0600

    Update hashing.cpp

--- hashing.cpp
+++ hashing.cpp
@@ -1,28 +1,37 @@
-#include <bits/stdc++.h>
-using namespace std;
-struct Hash {
-    static const int M1=1e9+7, M2=1e9+9, B1=29, B2=31; // change mod and base accordingly, use prime numbers
-    vector< pair<int, int> > hashes, expo;
-    Hash(string& s) {
-        int n = s.size();
-        // hash array is 1 based
-        hashes.assign(n+1, {0,0});
-        expo.assign(n+1, {1,1});
-        for(int i=0;i<n;i++) {
-            hashes[i+1] = { ((1LL * hashes[i].first * B1 % M1) + s[i]) % M1 ,
-                            ((1LL * hashes[i].second * B2 % M2) + s[i]) % M2 };
-            expo[i+1] = { 1LL * expo[i].first * B1 % M1 ,
-                            1LL * expo[i].second * B2 % M2 };
-        }
-    }
+class RollingHash {
+public:
+	int n;
+	static const ll MOD1 = 1e9 + 7, MOD2 = 1e9 + 9, base = 43;
+	vector<int> fwd1, fwd2, rev1, rev2, pow1, pow2;
 
-    // call with 0 based indexing
-    pair<int, int> get(int l, int r) {
-        ++l, ++r;
-        int h1 = hashes[r].first - (1LL * hashes[l-1].first * expo[r-l+1].first % M1);
-        ((h1%=M1)+=M1)%=M1;
-        int h2 = hashes[r].second - (1LL * hashes[l-1].second * expo[r-l+1].second % M2);
-        ((h2%=M2)+=M2)%=M2;
-        return {h1, h2};
-    }
+	RollingHash(const string &s) {
+		n = s.size();
+		fwd1.resize(n + 1, 0);
+		fwd2 = rev1 = rev2 = fwd1;
+		pow1.resize(n + 1, 1);
+		pow2 = pow1;
+
+		for (int i = 0; i < n; i++) {
+			pow1[i + 1] = (pow1[i] * base) % MOD1;
+			pow2[i + 1] = (pow2[i] * base) % MOD2;
+			fwd1[i + 1] = (fwd1[i] * base + s[i]) % MOD1;
+			fwd2[i + 1] = (fwd2[i] * base + s[i]) % MOD2;
+		}
+		for (int i = n - 1; i >= 0; i--) {
+			rev1[i] = (rev1[i + 1] * base + s[i]) % MOD1;
+			rev2[i] = (rev2[i + 1] * base + s[i]) % MOD2;
+		}
+	}
+	// call with 0 index
+	pair<int, int> getHash(int l, int r) {
+		int h1 = (fwd1[r + 1] - (fwd1[l] * 1LL * pow1[r - l + 1]) % MOD1 + MOD1) % MOD1;
+		int h2 = (fwd2[r + 1] - (fwd2[l] * 1LL * pow2[r - l + 1]) % MOD2 + MOD2) % MOD2;
+		return {h1, h2};
+	}
+	// call with 0 index
+	pair<int, int> getReverseHash(int l, int r) {
+		ll h1 = (rev1[l] - (rev1[r + 1] * 1LL * pow1[r - l + 1]) % MOD1 + MOD1) % MOD1;
+		ll h2 = (rev2[l] - (rev2[r + 1] * 1LL * pow2[r - l + 1]) % MOD2 + MOD2) % MOD2;
+		return {h1, h2};
+	}
 };
