6580cdba7034ba5768f2e4fa4fad7d0789cd4af6
Md. Arifur Rahman <113102124+rahman-arifur@users.noreply.github.com>
2025-09-12 02:18:23 +0600

    Update convex hull.cpp

--- convex hull.cpp
+++ convex hull.cpp
@@ -1,18 +1,34 @@
 #include "bits/stdc++.h"
 using namespace std;
+using ll = long long;
+using ld = long double;
+const ld PI = acosl(-1);
+short sign(ll x) { return (x > 0) - (x < 0); }
 struct Point {
   typedef Point P;
-  typedef long long T;
+  typedef long double T;
   T x, y;
   Point(T x=0, T y=0) : x(x), y(y) {}
-  bool operator<(P& a)const{ return x==a.x?y<a.y:x<a.x; }
-  bool operator==(P& a)const{ return x==a.x and y==a.y; }
+  bool operator<(P& p) const { return tie(x,y) < tie(p.x,p.y); }
+  bool operator==(P& p) const { return tie(x,y)==tie(p.x,p.y); }
+  P operator+(P& p) const { return P(x+p.x, y+p.y); }
   P operator-(P p) const { return P(x-p.x, y-p.y); }
+  P operator*(T d) const { return P(x*d, y*d); }
+  P operator/(T d) const { return P(x/d, y/d); }
   T dot(P p) const { return x*p.x + y*p.y; }
   T cross(P p) const { return x*p.y - y*p.x; }
-  friend istream& operator>>(istream&in,P&p) {
-    return in>>p.x>>p.y;
-  }
+  T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+  T dist2() const { return x*x + y*y; }
+  ld dist() const { return sqrtl(dist2()); }
+  // angle to x-axis in interval [-pi, pi]
+  ld angle() const { return atan2l(y, x); }
+  P unit() const { return *this/dist(); } // makes dist()=1
+  P perp() const { return P(-y, x); } // rotates +90 degrees
+  P normal() const { return perp().unit(); }
+  // returns point rotated 'a' radians ccw around the origin
+  P rotate(long double a) const {return P(x*cosl(a)-y*sinl(a),x*sinl(a)+y*cosl(a)); }
+  friend ostream& operator<<(ostream& os, P p) {return os << p.x << ' ' << p.y << '\n';}
+  friend istream& operator>>(istream&in,P&p) {return in>>p.x>>p.y;}
 };
 vector<Point> convex_hull(vector<Point> points) {
     sort(points.begin(), points.end());
@@ -34,16 +50,30 @@ vector<Point> convex_hull(vector<Point> points) {
     hull.pop_back();
     return hull;
 }
-signed main() {
-    cin.tie(0)->sync_with_stdio(0);
-    int n;
-    for(cin>>n;n;cin>>n) {
-        vector<Point> v(n);
-        for(int i=0;i<n;i++)cin>>v[i];
-        v = convex_hull(v);
-        n = v.size();
-        cout<<n<<'\n';
-        // https://open.kattis.com/problems/convexhull
-        for(int i=0;i<n;i++)cout<<v[i].x<<' '<<v[i].y<<'\n';
+ld diameter(vector<Point>& v) {
+    int n = v.size();
+    if(n==1)return 0;
+    if(n==2)return (v[0]-v[1]).dist();
+    ld ans = 0;
+    int i=0, j=1;
+    while(i<n) {
+        while((v[(i+1)%n]-v[i]).cross(v[(j+1)%n]-v[j])>=0) {
+            ans = max(ans, (v[i]-v[j]).dist2());
+            j=(j+1)%n;
+        }
+        ans = max(ans, (v[i]-v[j]).dist2());
+        i++;
     }
+    return sqrtl(ans);
 }
+signed main() 
+{
+    cin.tie(0)->sync_with_stdio(0);
+    int n;cin>>n;
+    vector<Point> v(n);
+    for(auto& u:v)cin>>u;
+    v = convex_hull(v);
+    cout<<setprecision(20)<<diameter(v);
+    return 0;
+}
+// if we keep holding onto yesterday, what are we going to be tomorrow?
