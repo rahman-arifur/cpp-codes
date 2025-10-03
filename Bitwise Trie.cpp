586fb57b7014f9daf95712a23b36353161a195ba
Md. Arifur Rahman <113102124+rahman-arifur@users.noreply.github.com>
2025-10-03 19:24:18 +0600

    Update Bitwise Trie.cpp

--- Bitwise Trie.cpp
+++ Bitwise Trie.cpp
@@ -1,79 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 struct TrieNode {
-    TrieNode* child[2];
-    TrieNode() {child[0] = child[1] = 0;}
+	TrieNode* child[2];
+	int cnt;  // number of values passing through this node
+	TrieNode() {
+		child[0] = child[1] = 0;
+		cnt = 0;
+	}
 };
 class BitwiseTrie {
 public:
-    BitwiseTrie() {root = new TrieNode();}
-    // void clr(TrieNode* rr) {
-    //     if (rr == 0)return;
-    //     clr(rr->child[0]);
-    //     clr(rr->child[1]);
-    //     delete rr;
-    // }
-    // ~BitwiseTrie() {clr(root);}
-    // Insert number into trie
-    void insert(int num) {
-        TrieNode* cur = root;
-        // from bit 31 (MSB) down to 0 (LSB)
-        for (int b = 31; b >= 0; --b) {
-            int bit = (num >> b) & 1;
-            if (!cur->child[bit]) cur->child[bit] = new TrieNode();
-            cur = cur->child[bit];
-        }
-    }
-    int queryMinXor(int num) {
-        TrieNode* cur = root;
-        int xorVal = 0;
-        for (int b = 31; b >= 0; --b) {
-            int bit = (num >> b) & 1;
-            // prefer to go to same bit to keep XOR 0 at this position
-            if (cur->child[bit])cur = cur->child[bit];
-            else {
-                cur = cur->child[bit ^ 1];
-                xorVal |= (1 << b);
-            }
-        }
-        return xorVal;
-    }
-    int queryMaxXor(int num) {
-        TrieNode* cur = root;
-        int xorVal = 0;
-        for (int b = 31; b >= 0; --b) {
-            int bit = (num >> b) & 1;
-            if (cur->child[bit ^ 1]) {
-                cur = cur->child[bit ^ 1];
-                xorVal |= (1 << b);
-            } else cur = cur->child[bit];
-        }
-        return xorVal;
-    }
+	const int B = 30;  // 62 for ll
+	using T = int;	   // make long long
+	BitwiseTrie() { root = new TrieNode(); }
+
+	// void clr(TrieNode* rr) {
+	//     if (rr == 0)return;
+	//     clr(rr->child[0]);
+	//     clr(rr->child[1]);
+	//     delete rr;
+	// }
+	// ~BitwiseTrie() {clr(root);}
+
+	void insert(T num) {
+		TrieNode* cur = root;
+		cur->cnt++;
+		for (int b = B; b >= 0; --b) {
+			int bit = (num >> b) & 1;
+			if (!cur->child[bit]) cur->child[bit] = new TrieNode();
+			cur = cur->child[bit];
+			cur->cnt++;
+		}
+	}
+	void remove(int num) {
+		TrieNode* cur = root;
+		for (int b = B; b >= 0; --b) {
+			int bit = (num >> b) & 1;
+			if (!cur->child[bit] || cur->child[bit]->cnt == 0) return;
+			cur = cur->child[bit];
+			cur->cnt--;
+		}
+	}
+	T queryMaxXor(T num) const {
+		TrieNode* cur = root;
+		if (!cur || cur->cnt == 0) return 0;
+		T xorVal = 0;
+		for (int b = B; b >= 0; --b) {
+			int bit = (num >> b) & 1;
+			int want = bit ^ 1;
+			if (cur->child[want] && cur->child[want]->cnt > 0) {
+				xorVal |= (1LL << b);
+				cur = cur->child[want];
+			} else if (cur->child[bit] && cur->child[bit]->cnt > 0)
+				cur = cur->child[bit];
+			else
+				break;
+		}
+		return xorVal;
+	}
 
 private:
-    TrieNode* root;
+	TrieNode* root;
 };
-
-int findMinXORWithTrie(const vector<int>& nums) {
-    if (nums.size() < 2) return 0;
-    BitwiseTrie trie;
-    trie.insert(nums[0]);
-    int minXor = INT_MAX;
-    for (size_t i = 1; i < nums.size(); ++i) {
-        int curXor = trie.queryMinXor(nums[i]);
-        minXor = min(minXor, curXor);
-        trie.insert(nums[i]);
-    }
-    return minXor;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    vector<int> nums = {9, 5, 3, 10};
-    cout << "Minimum XOR is: " << findMinXORWithTrie(nums) << "\n";
-    return 0;
-}
