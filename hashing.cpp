14471d4304b1d644eeced989f185d2dc7890c7ae
tophhh <arifurr946@gmail.com>
2025-11-19 00:25:07 +0600

    edited hash

--- hashing.cpp
+++ hashing.cpp
@@ -1,38 +1,44 @@
 class RollingHash {
 public:
-// base, mod = (1949313259LL, 2091573227LL), (1997293877, 2117566807)
+// B1, mod = (1949313259LL, 2091573227LL), (1997293877, 2117566807)
 	int n;
-	static const ll MOD1 = 1e9 + 7, MOD2 = 1e9 + 9, base = 43;
-	vector<int> fwd1, fwd2, rev1, rev2, pow1, pow2;
+	static const ll MOD1 = 1e9 + 7, MOD2 = 1e9 + 9, B1 = 43, B2 = 103;
+	static vector<int> pow1, pow2;
+	vector<int> fwd1, fwd2, rev1, rev2;
 
 	RollingHash(const string &s) {
+		if(pow1.empty()) {
+			pow1 = pow2 = vector<int> (mxn, 1);
+			for(int i=1;i<mxn;i++) {
+				pow1[i] = B1 * pow1[i-1] % MOD1;
+    				pow2[i] = B2 * pow2[i-1] % MOD2;
+			}
+		}
 		n = s.size();
 		fwd1.resize(n + 1, 0);
 		fwd2 = rev1 = rev2 = fwd1;
-		pow1.resize(n + 1, 1);
-		pow2 = pow1;
 
 		for (int i = 0; i < n; i++) {
-			pow1[i + 1] = (pow1[i] * base) % MOD1;
-			pow2[i + 1] = (pow2[i] * base) % MOD2;
-			fwd1[i + 1] = (fwd1[i] * base + s[i]) % MOD1;
-			fwd2[i + 1] = (fwd2[i] * base + s[i]) % MOD2;
+			fwd1[i + 1] = (fwd1[i] * B1 + s[i]) % MOD1;
+			fwd2[i + 1] = (fwd2[i] * B2 + s[i]) % MOD2;
 		}
 		for (int i = n - 1; i >= 0; i--) {
-			rev1[i] = (rev1[i + 1] * base + s[i]) % MOD1;
-			rev2[i] = (rev2[i + 1] * base + s[i]) % MOD2;
+			rev1[i] = (rev1[i + 1] * B1 + s[i]) % MOD1;
+			rev2[i] = (rev2[i + 1] * B2 + s[i]) % MOD2;
 		}
 	}
 	// call with 0 index
 	pair<int, int> getHash(int l, int r) {
-		int h1 = (fwd1[r + 1] - (fwd1[l] * 1LL * pow1[r - l + 1]) % MOD1 + MOD1) % MOD1;
-		int h2 = (fwd2[r + 1] - (fwd2[l] * 1LL * pow2[r - l + 1]) % MOD2 + MOD2) % MOD2;
+		++r;
+		int h1 = (fwd1[r] - (fwd1[l] * 1LL * pow1[r - l]) % MOD1 + MOD1) % MOD1;
+		int h2 = (fwd2[r] - (fwd2[l] * 1LL * pow2[r - l]) % MOD2 + MOD2) % MOD2;
 		return {h1, h2};
 	}
 	// call with 0 index
 	pair<int, int> getReverseHash(int l, int r) {
-		ll h1 = (rev1[l] - (rev1[r + 1] * 1LL * pow1[r - l + 1]) % MOD1 + MOD1) % MOD1;
-		ll h2 = (rev2[l] - (rev2[r + 1] * 1LL * pow2[r - l + 1]) % MOD2 + MOD2) % MOD2;
+		++r;
+		ll h1 = (rev1[l] - (rev1[r] * 1LL * pow1[r - l]) % MOD1 + MOD1) % MOD1;
+		ll h2 = (rev2[l] - (rev2[r] * 1LL * pow2[r - l]) % MOD2 + MOD2) % MOD2;
 		return {h1, h2};
 	}
 };
