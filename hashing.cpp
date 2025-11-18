1dfc406a139e08f9e1763dc718335e38f0585aed
Md. Arifur Rahman <113102124+rahman-arifur@users.noreply.github.com>
2025-11-19 00:35:56 +0600

    hash edit

--- hashing.cpp
+++ hashing.cpp
@@ -1,23 +1,15 @@
+const int mxn = 100005;
+int pow1[mxn], pow2[mxn];
+static const ll MOD1 = 1e9 + 7, MOD2 = 1e9 + 9, B1 = 43, B2 = 103;
 class RollingHash {
+	int n;
+	vector<int> fwd1, fwd2, rev1, rev2;
 public:
 // B1, mod = (1949313259LL, 2091573227LL), (1997293877, 2117566807)
-	int n;
-	static const ll MOD1 = 1e9 + 7, MOD2 = 1e9 + 9, B1 = 43, B2 = 103;
-	static vector<int> pow1, pow2;
-	vector<int> fwd1, fwd2, rev1, rev2;
-
 	RollingHash(const string &s) {
-		if(pow1.empty()) {
-			pow1 = pow2 = vector<int> (mxn, 1);
-			for(int i=1;i<mxn;i++) {
-				pow1[i] = B1 * pow1[i-1] % MOD1;
-    				pow2[i] = B2 * pow2[i-1] % MOD2;
-			}
-		}
 		n = s.size();
 		fwd1.resize(n + 1, 0);
 		fwd2 = rev1 = rev2 = fwd1;
-
 		for (int i = 0; i < n; i++) {
 			fwd1[i + 1] = (fwd1[i] * B1 + s[i]) % MOD1;
 			fwd2[i + 1] = (fwd2[i] * B2 + s[i]) % MOD2;
@@ -42,3 +34,10 @@ public:
 		return {h1, h2};
 	}
 };
+int main() {
+	for(int i=1;i<mxn;i++) {
+    	pow1[i] = B1 * pow1[i-1] % MOD1;
+    	pow2[i] = B2 * pow2[i-1] % MOD2;
+  	}
+  return 0;
+}
