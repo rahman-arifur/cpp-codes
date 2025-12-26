d065abbb753ee8f6ae45b07028d4b372c0c0307a
tophhh <arifurr946@gmail.com>
2025-12-26 10:38:44 +0600

    updated lca

--- lca, binary lifting.cpp
+++ lca, binary lifting.cpp
@@ -1,24 +1,51 @@
-const int N= 2e5+1;
-int up[N][int(ceil(log2(N))) + 1];
-vector<int> adj[N];
-int tin[N], tout[N], ans[N], timer, LG, N;
-void dfs(int v, int p) {
-    tin[v] = ++timer;
-    up[v][0] = p;
-    for (int i = 1; i <= LG; ++i)
-        up[v][i] = up[up[v][i-1]][i-1];
-    for (int u : adj[v]) if (u != p)dfs(u, v);
-    tout[v] = ++timer;
+const int Lg = 20;
+vector<int>adj[mx];
+int level[mx], tin[N], tout[N], timer;
+int dp[Lg][mx];
+void dfs(int u) {
+  tin[v] = ++timer;
+  for (int i = 1;i < Lg;i++)
+    dp[i][u] = dp[i - 1][dp[i - 1][u]];
+  for (int v : adj[u]) {
+    if (dp[0][u] == v)continue;
+    level[v] = level[u] + 1;
+    dp[0][v] = u;
+    dfs(v);
+  }
+  tout[v] = ++timer;
 }
-bool is_ancestor(int u, int v) {
+bool is_ancestor(int u, int v) { // is u anc of v
     return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
 int lca(int u, int v) {
-    if (is_ancestor(u, v)) return u;
-    if (is_ancestor(v, u)) return v;
-    for (int i = LG; i >= 0; --i) {
-        if (!is_ancestor(up[u][i], v))
-            u = up[u][i];
-    }
-    return up[u][0];
+  if (level[v] < level[u])swap(u, v);
+  int diff = level[v] - level[u];
+  for (int i = 0;i < Lg;i++)
+    if (diff & (1 << i))
+      v = dp[i][v];
+  for (int i = Lg - 1;i >= 0;i--)
+    if (dp[i][u] != dp[i][v])
+      u = dp[i][u], v = dp[i][v];
+  return u == v ? u : dp[0][u];
 }
+int kth(int u, int k) {
+  for (int i = Lg - 1;i >= 0;i--)
+    if (k & (1 << i))
+      u = dp[i][u];
+  return u;
+}
+//kth node from u to v. 0th is u.
+int go(int u, int v, int k) {
+  int l = lca(u, v);
+  int d = level[u] + level[v] - (level[l] << 1);
+  assert(k <= d);
+  if (level[l] + k <= level[u]) return kth(u, k);
+  k -= level[u] - level[l];
+  return kth(v, level[v] - level[l] - k);
+}
+/*
+   LCA(u,v) with root r:
+   lca(u,v)^lca(u,r)^lca(v,r)
+   Distance between u,v:
+   level(u) + level(v) - 2*level(lca(u,v))
+   */
