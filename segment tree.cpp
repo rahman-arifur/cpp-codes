052a6ef241b3e09ec71e52ad047d0e10cf3001ab
rahman-arifur <arifurr946@gmail.com>
2026-02-18 15:07:00 +0600

    bleh

--- segment tree.cpp
+++ segment tree.cpp
@@ -14,8 +14,7 @@ public:
 		tree.resize(n << 1, E);
 	}
 	void update(int ind, T val) {
-		ind += n;
-		tree[ind] = val;
+		tree[ind += n] = val;
 		while (ind > 1) {
 			ind /= 2;
 			tree[ind] = func(tree[ind << 1], tree[(ind << 1) | 1]);
