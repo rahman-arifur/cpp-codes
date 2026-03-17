98d79eb7ff38c55e1be5a479e29ac928cbce16f4
Md. Arifur Rahman <113102124+rahman-arifur@users.noreply.github.com>
2026-03-18 05:04:11 +0600

    Add Mobius function calculation to linear sieve

--- linear sieve.cpp
+++ linear sieve.cpp
@@ -1,14 +1,20 @@
 int spf[mxn];
+int mu[mxn];
 vector<int> prm;
+
+mu[1] = 1;
 for (int i = 2; i < mxn; ++i) {
 	if (spf[i] == 0) {
 		spf[i] = i;
+		mu[i] = -1;
 		prm.emplace_back(i);
 	}
 	for (int j = 0; i * prm[j] < mxn; ++j) {
 		spf[i * prm[j]] = prm[j];
 		if (prm[j] == spf[i]) {
+			mu[i * prm[j]] = 0;
 			break;
 		}
+		mu[i * prm[j]] = -mu[i];
 	}
 }
