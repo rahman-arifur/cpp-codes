a1c7958484a7b1bb1e69465187dbc03a7fa20f93
Md. Arifur Rahman <113102124+rahman-arifur@users.noreply.github.com>
2026-08-21 14:27:47 +0600

    Refactor SegmentTree implementation with Node struct

--- segment tree.cpp
+++ segment tree.cpp
@@ -1,32 +1,38 @@
+struct Node {
+    long long val;
+    Node(long long v = 0) : val(v) {}
+    friend Node merge(const Node& a, const Node& b) {
+        return Node(a.val + b.val); 
+    }
+};
+
 template <typename T>
-class SegmentTree {
-private:  // all 1 index
-	int n;
-	vector<T> tree;
-	T E;					   // null value
-	function<T(T&, T&)> func;  // merge function
-public:
-	SegmentTree(int _n, T init, function<T(T&, T&)> ff) {
-		n = 1;
-		E = init;
-		func = ff;
-		while (n < _n) n <<= 1;
-		tree.resize(n << 1, E);
-	}
-	void update(int ind, T val) {
-		tree[ind += n] = val;
-		while (ind > 1) {
-			ind /= 2;
-			tree[ind] = func(tree[ind << 1], tree[(ind << 1) | 1]);
-		}
-	}
-	T query(int l, int r) {
-		T res = E;
-		for (l += n, r += n; l <= r; l >>= 1, r >>= 1) {
-			if (l & 1) res = func(res, tree[l]);
-			if (!(1 & r)) res = func(res, tree[r]);
-			++l, --r;
-		}
-		return res;
-	}
+struct SegmentTree {
+    int n;
+    vector<T> tree;
+    T E;
+
+    SegmentTree(int _n, T neutral = T()) {
+        n = 1;
+        while (n < _n) n <<= 1; 
+        E = neutral;
+        tree.assign((n << 1) + 2, E); 
+    }
+
+    void update(int ind, const T& val) {
+        ind += n;
+        tree[ind] = val;
+        for (ind >>= 1; ind > 0; ind >>= 1) {
+            tree[ind] = merge(tree[ind << 1], tree[ind << 1 | 1]);
+        }
+    }
+
+    T query(int l, int r) {
+        T resL = E, resR = E;
+        for (l += n, r += n; l <= r; l >>= 1, r >>= 1) {
+            if (l & 1) resL = merge(resL, tree[l++]);
+            if (!(r & 1)) resR = merge(tree[r--], resR); 
+        }
+        return merge(resL, resR);
+    }
 };
